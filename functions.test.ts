2b5c586c20abfeeccd70b48382a9161aad8e2728
Austin Holmes <austin.holmes4@gmail.com>
2022-11-04 11:12:41 -0600

    initial commit

---
+++ functions.test.ts
@@ -0,0 +1,11 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    test('shuffleArray should return an array', () => {
+        expect(shuffleArray).toBe([])
+    },
+
+    test('', () => {
+        expect(shuffleArray([i])).toHaveLength(shuffleArray.length)
+    })
+})
